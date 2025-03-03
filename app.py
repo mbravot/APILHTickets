1366cfb43cfb4d7293d14821cf4238b572a0e1d1
Tu Nombre <tuemail@example.com>
2025-03-03 13:48:54 -0300

    se modifica para despliegue de prueba en azure

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080)
 
 
     # 🔹 Configuración de la base de datos
